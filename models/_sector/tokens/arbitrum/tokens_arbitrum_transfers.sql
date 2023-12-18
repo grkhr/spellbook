8259659ad75e02a583a92618458f75ae3a9ce0b0
grkhr <eduardgorkh@gmail.com>
2023-12-18 17:24:50 +0200

    try bnb

--- models/_sector/tokens/arbitrum/tokens_arbitrum_transfers.sql
+++ models/_sector/tokens/arbitrum/tokens_arbitrum_transfers.sql
@@ -1,17 +1,12 @@
 {{config(
     schema = 'tokens_arbitrum',
     alias = 'transfers',
-    partition_by = ['token_standard', 'block_date'],
-    materialized = 'incremental',
-    file_format = 'delta',
-    incremental_strategy = 'merge',
-    incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')],
-    unique_key = ['unique_key'],
+    materialized = 'view',
 )
 }}
 
 {{transfers_enrich(
     blockchain='arbitrum',
     transfers_base = ref('tokens_arbitrum_base_transfers'),
-    native_symbol = 'ETH'
+    native_symbol = 'AVAX'
 )}}
--- models/_sector/tokens/bnb/tokens_bnb_transfers.sql
+++ models/_sector/tokens/bnb/tokens_bnb_transfers.sql
@@ -1,12 +1,17 @@
 {{config(
     schema = 'tokens_bnb',
     alias = 'transfers',
-    materialized = 'view',
+    partition_by = ['token_standard', 'block_date'],
+    materialized = 'incremental',
+    file_format = 'delta',
+    incremental_strategy = 'merge',
+    incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_time')],
+    unique_key = ['unique_key'],
 )
 }}
 
 {{transfers_enrich(
     blockchain='bnb',
     transfers_base = ref('tokens_bnb_base_transfers'),
-    native_symbol = 'BNB'
+    native_symbol = 'ETH'
 )}}
